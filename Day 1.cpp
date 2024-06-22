73b61a919b09918abc26d86cb5019e5107d8035e
Nikhil_1221 <Nikhilkumarsinghpbic@gmail.com>
2024-06-23 02:49:50 +0530

    Day 1

--- Day 1.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	cout<<"Nikhil Kumar singh";
-}
---
+++ Namaste_Duniya.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Namaste Duniya"<<endl;
+    int a=12;
+    cout<<"Size of "<<a<<" is "<<sizeof(a)<<endl;
+    char b='b';
+    cout<<"Size of "<<b<<" is "<<sizeof(b)<<endl;
+    bool c=true;
+    cout<<"Size of "<<c<<" is "<<sizeof(c)<<endl;
+    float f=1.2;
+    cout<<"Size of "<<f<<" is "<<sizeof(f)<<endl;
+    double d = 1.23;
+    cout<<"Size of "<<d<<" is "<<sizeof(d)<<endl;
+
+    int tc='a';
+    cout<<tc<<endl;
+    char tc1=98;
+    cout<<tc1<<endl;
+    char tc2=12356;
+    cout<<tc2<<endl;
+
+
+    unsigned us=123;
+    cout<<us<<endl;
+    unsigned us1=-2;
+    cout<<us1<<endl;
+
+
+    cout<<24/2.5<<endl;
+
+    int r1,r2,x=2,y=5;
+    r1=x==y;
+    r2=x<y;
+    cout<<r1<<endl;
+    cout<<r2<<endl;
+
+}
