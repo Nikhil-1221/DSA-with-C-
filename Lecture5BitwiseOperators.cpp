ee9ba8b18f8ff026f953d14b04d74eb89c7bec6d
Nikhil_1221 <Nikhilkumarsinghpbic@gmail.com>
2024-07-04 23:55:02 +0530

    Day 8

---
+++ Lecture5BitwiseOperators.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // Operator
+    int a=2;
+    int b=3;
+    // And Operator
+    cout<<"a&b "<<(a&b)<<endl;
+    //Or Operator
+    cout<<"a|b "<<(a|b)<<endl;
+    // NOT Operator
+    cout<<"~a"<<(~a)<<"         "<<"~b "<<(~b)<<endl;
+    // Xor Operator
+    cout<<"a^b "<<(a^b)<<endl;
+    
+}
