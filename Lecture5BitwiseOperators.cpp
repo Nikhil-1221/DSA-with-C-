c996930f6718b6c206e28d43d333958a898adef4
Nikhil_1221 <Nikhilkumarsinghpbic@gmail.com>
2024-07-05 00:39:18 +0530

    Day 9

--- Lecture5BitwiseOperators.cpp
+++ Lecture5BitwiseOperators.cpp
@@ -5,12 +5,15 @@ int main(){
     int a=2;
     int b=3;
     // And Operator
-    cout<<"a&b "<<(a&b)<<endl;
+    cout<<"a&b => "<<(a&b)<<endl;
     //Or Operator
-    cout<<"a|b "<<(a|b)<<endl;
+    cout<<"a|b => "<<(a|b)<<endl;
     // NOT Operator
-    cout<<"~a"<<(~a)<<"         "<<"~b "<<(~b)<<endl;
+    cout<<"~a => "<<(~a)<<"         "<<"~b => "<<(~b)<<endl;
     // Xor Operator
-    cout<<"a^b "<<(a^b)<<endl;
-    
+    cout<<"a^b =>"<<(a^b)<<endl;
+    //Left Shift
+    cout<<"a<<1 => "<<(a<<1)<<"       "<<"a<<2 => "<<(a<<2)<<endl;
+    // Right Shift
+    cout<<"a>>1 => "<<(a>>1)<<"       "<<"a>>2 => "<<(a>>2)<<endl;
 }
